05b9dcd0679b44c7034ef11e2aa964979400e047
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-05-12 14:21:59 +0700

    add Enum

---
+++ app/Enums/AcademicYearSemester.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum AcademicYearSemester: string {
+    case ODD = 'Ganjil';
+    case EVEN = 'GENAP';
+
+
+    public static function options() {
+        return collect(self::cases())->map(fn($item) => [
+            'value' => $item->value,
+            'label' => $item->value,
+        ])->values()->toArray();
+    }
+}
